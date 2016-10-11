c222476ec5244c44c34e31beaf641402064d1a93
“Matteo git config --global user.email “matteo.soresini@hotmail.it <matteo.soresini@hotmail.it>
2016-10-11 13:52:14 +0200

    trying some style for the fish

--- js/js.js
+++ js/js.js
@@ -1,8 +1,26 @@
 $(document).ready(function() {
 
-   $(".squares").on('click', function(){
+	var player = 1;
+
+   $(".squares").on('click', function(event){
 
       alert("working");
+      var squareSelected = $(this);
+
+      if(squareSelected.hasClass("X") || squareSelected.hasClass("O")) {
+      	alert("This square has already been selected! Choose another one :-).")
+      } else {
+
+      	if (player === 1) {
+      		squareSelected.addClass("X");
+      		player = 2;
+
+        } else {
+        	squareSelected.addClass("O");
+        	player = 1;
+        }
+
+      }
 
 
    });
